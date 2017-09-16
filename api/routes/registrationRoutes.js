a93bf73c34d6774e2abd2f49b3574776a18f5fde
Dat Bac Do <Dat Do dod2@wit.edu>
2017-09-16 19:25:42 -0400

    allow access control origin

--- api/routes/registrationRoutes.js
+++ api/routes/registrationRoutes.js
@@ -2,6 +2,12 @@
 module.exports = function(app) {
   const userList = require('../controllers/registrationController');
 
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
   // userList Routes
   app.route('/users')
     .get(userList.get_all_users)
